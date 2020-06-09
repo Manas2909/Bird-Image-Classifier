56afc31eb5c272007b9a44b8223113535f8f59bd
Manas2909 <manaskmukul@gmail.com>
2020-06-09 14:36:33 +0530

    Update predtest.py

--- predtest.py
+++ predtest.py
@@ -4,7 +4,7 @@ import numpy as np
 from keras.models import load_model
 from keras.preprocessing import image
 model = load_model('model.h5')
-test_image = image.load_img('C:\\Users\\Manas\\OneDrive\\Pictures\\Bird Photo\\Train\\Crow\\images.jpg', target_size = (150, 150))
+test_image = image.load_img('C:\\Users\\Manas\\OneDrive\\Pictures\\Bird Photo\\Test\\Crow\\images.jpg', target_size = (150, 150))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = model.predict(test_image)
